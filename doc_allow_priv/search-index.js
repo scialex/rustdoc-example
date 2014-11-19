12d7c3165b4d7efff622b26fe1c6d066d13d693f
Alexander Light <allight@cs.brown.edu>
2014-11-19 11:14:10 -0500

    pages

---
+++ doc_allow_priv/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = {};
+searchIndex['tester'] = {"items":[[0,"","tester",""],[3,"exp","","This is exported\n"],[0,"abc","","The abc mod\n"],[3,"def","tester::abc","This is public\n"],[3,"hij","","This is not\n"]],"paths":[]};
+searchIndex['lib'] = {"items":[[0,"","lib",""],[3,"exp","","This is exported\n"],[0,"abc","","The abc mod\n"],[3,"def","lib::abc","This is public\n"],[3,"hij","","This is not\n"]],"paths":[]};
+
+initSearch(searchIndex);
---
+++ doc_default/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['tester'] = {"items":[[0,"","tester",""],[3,"exp","","This is exported"],[0,"abc","","The abc mod"],[3,"def","tester::abc","This is public"]],"paths":[]};
+initSearch(searchIndex);
